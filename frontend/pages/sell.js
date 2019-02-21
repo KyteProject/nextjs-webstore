e645608150a49636168034771bfdc949d6ee305e
KyteProject <headhertz88@gmail.com>
2019-02-21 00:15:15 +0000

    more setup

---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = () => (
+	<React.Fragment>
+		<Link href='/'>
+			<button as='a'>test</button>
+		</Link>
+	</React.Fragment>
+);
+
+export default Sell;
