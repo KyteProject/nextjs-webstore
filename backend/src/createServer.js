701d930d4fbe67b96186c9a0b3c220e62e292a4e
Dan <headhertz88@gmail.com>
2019-02-25 22:06:29 +0000

    basic item mutation/query

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,5 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
-import Mutation from './resolvers/Mutation';
+import Mutations from './resolvers/Mutation';
 import Query from './resolvers/Query';
 import db from './db';
 
@@ -7,7 +7,7 @@ const createServer = () => {
 	return new GraphQLServer( {
 		typeDefs: './src/schema.graphql',
 		resolvers: {
-			Mutation,
+			Mutations,
 			Query
 		},
 		resolverValidationOptions: {
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutation = {};
+const Mutations = {
+	async createItem( parent, args, ctx, info ) {
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
 
-export default Mutation;
+		return item;
+	}
+};
+
+export default Mutations;
